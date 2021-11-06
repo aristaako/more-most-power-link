9d4d96172f56f4acf418473ee150c80b33b5cbbd
Ari Rauhala <arirauhala@gmail.com>
2021-11-06 14:26:49 +0200

    Sets API_URL to be read from an env variable if present

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 import { MostPowerLinkStationData } from '../common/types'
 
-const API_URL = 'http://localhost:9000/link'
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:9000'
 
 export const getMostPowerLinkStations = async (
   pathParams: string
 ): Promise<MostPowerLinkStationData> => {
   return axios
-    .get(`${API_URL}${pathParams}`)
+    .get(`${API_URL}/link${pathParams}`)
     .then((response) => {
       const status = 200
       const stations = response.data
